b2a6666a976717c515ce6a26ff64774fea453b16
Joao Pinto <jdpinto@me.local>
2020-04-16 19:00:59 +0100

    discard my changes"

--- appointments/models.py
+++ appointments/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     image = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return f'{self.title} - {self.author}'
+
+
+class Appointments(models.Model):
+    appointment_date = models.DateTimeField()
+    # User = get_user_model()
+    service = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.service}'
+
+
+class Services(models.Model):
+    service_name = models.CharField(max_length=500)
+
+  def __
